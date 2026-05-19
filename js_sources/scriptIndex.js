4c350fb234a896c75529deb5fea8c516aa99ec34
Mclorenze <78360225+Mclorl@users.noreply.github.com>
2026-05-20 00:11:23 +0800

    Update scriptIndex.js

--- js_sources/scriptIndex.js
+++ js_sources/scriptIndex.js
@@ -245,4 +245,15 @@ window.addEventListener('resize', function() {
     renderer.setSize(container.clientWidth, container.clientHeight);
 });
 
+/* toggle menu for homepage nav */
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.querySelector('.nav-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    toggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        toggle.classList.toggle('active');
+    });
+});
+
 
